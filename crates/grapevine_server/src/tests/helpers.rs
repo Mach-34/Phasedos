6c0ded59900a94f566cd608bb07143ce37cef923
Ian-Bright <ianbrighton1@gmail.com>
2024-08-02 09:16:51 -0600

    feat: Server tests reorganized and passing

--- crates/grapevine_server/src/tests/helpers.rs
+++ crates/grapevine_server/src/tests/helpers.rs
@@ -1,13 +1,43 @@
-use crate::{catchers::bad_request, health, mongo::GrapevineDB, routes};
+use crate::{
+    catchers::bad_request,
+    health,
+    mongo::GrapevineDB,
+    routes,
+    tests::http::{
+        http_add_relationship, http_create_user, http_get_available_proofs, http_get_proving_data,
+        http_submit_degree_proof,
+    },
+    utils::{use_public_params, use_r1cs, use_wasm},
+};
+use babyjubjub_rs::{decompress_point, decompress_signature};
+use ff::PrimeField;
 use grapevine_circuits::{
-    inputs::GrapevineInputs,
+    inputs::{GrapevineArtifacts, GrapevineInputs, GrapevineOutputs},
+    nova::{degree_proof, verify_grapevine_proof},
     utils::{compress_proof, decompress_proof},
 };
-use grapevine_common::{account::GrapevineAccount, http::requests::CreateUserRequest, NovaProof};
+use grapevine_common::{
+    account::GrapevineAccount,
+    auth_secret::AuthSecretEncrypted,
+    http::requests::{CreateUserRequest, DegreeProofRequest},
+    models::ProvingData,
+    Fr, NovaProof,
+};
+use lazy_static::lazy_static;
 use rocket::{
     fs::{relative, FileServer},
     local::asynchronous::Client,
 };
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref USERS: Mutex<Vec<GrapevineAccount>> = Mutex::new(vec![]);
+    pub static ref ARTIFACTS: GrapevineArtifacts = GrapevineArtifacts {
+        params: use_public_params().unwrap(),
+        r1cs: use_r1cs().unwrap(),
+        wasm_path: use_wasm().unwrap()
+    };
+}
 
 pub struct GrapevineTestContext {
     pub client: Client,
@@ -45,8 +75,7 @@ pub fn build_identity_proof(from: &GrapevineAccount) -> NovaProof {
     // get inputs
     let private_key = &from.private_key();
     let identity_inputs = GrapevineInputs::identity_step(private_key);
-    grapevine_circuits::nova::identity_proof(&crate::test_rocket::ARTIFACTS, &identity_inputs)
-        .unwrap()
+    grapevine_circuits::nova::identity_proof(&*ARTIFACTS, &identity_inputs).unwrap()
 }
 
 /**
@@ -63,7 +92,7 @@ pub fn build_degree_inputs(
     degree: u8,
 ) -> (NovaProof, GrapevineInputs, GrapevineOutputs) {
     let mut proof = decompress_proof(&proving_data.proof[..]);
-    let res = verify_grapevine_proof(&proof, &ARTIFACTS.params, degree as usize)
+    let res = verify_grapevine_proof(&proof, &*&ARTIFACTS.params, degree as usize)
         .unwrap()
         .0;
     let outputs = GrapevineOutputs::try_from(res).unwrap();
@@ -89,6 +118,53 @@ pub fn build_degree_inputs(
     (proof, inputs, outputs)
 }
 
+/**
+ * Full degree proof step
+ *
+ * @param context - the mocked rocket http server context
+ * @param prover - the account proving the degree
+ * @param scope - Optionally the username of the scope to use when multiple available proofs, or the first one
+ * @return - http code and message from server for degree proof submission
+ */
+pub async fn degree_proof_step_by_scope(
+    context: &GrapevineTestContext,
+    prover: &mut GrapevineAccount,
+    scope: Option<&String>,
+) -> (u16, String) {
+    // select the specific proof to build from
+    let available_proofs = http_get_available_proofs(&context, prover).await;
+    let available_proof = match scope {
+        Some(scope) => available_proofs
+            .iter()
+            .find(|&proof| scope == &proof.scope)
+            .unwrap(),
+        None => available_proofs.first().unwrap(),
+    };
+    // retrieve proving data
+    let degree = available_proof.degree;
+    let proving_data =
+        http_get_proving_data(&context, prover, &available_proof.id.to_string()).await;
+    // parse
+    let (mut proof, inputs, outputs) = build_degree_inputs(prover, &proving_data, degree as u8);
+    // prove
+    degree_proof(
+        &*ARTIFACTS,
+        &inputs,
+        &mut proof,
+        &outputs.try_into().unwrap(),
+    )
+    .unwrap();
+    // build DegreeProofRequest
+    let compressed = compress_proof(&proof);
+    let degree_proof_request = DegreeProofRequest {
+        proof: compressed,
+        previous: available_proof.id.to_string(),
+        degree: degree + 1,
+    };
+    // submit degree proof and return result
+    http_submit_degree_proof(&context, prover, degree_proof_request).await
+}
+
 /// Request Helpers
 
 /**
@@ -111,4 +187,41 @@ pub fn build_create_user_request(from: &GrapevineAccount) -> CreateUserRequest {
 pub fn generate_nonce_signature(user: &GrapevineAccount) -> String {
     let nonce_signature = user.sign_nonce();
     hex::encode(nonce_signature.compress())
+}
+
+/**
+ * Generate a specified number of users and register them in the grapevine service
+ *
+ * @param num_users - the number of users to enroll
+ * @return  - the registered grapevine accounts
+ */
+pub async fn get_users(context: &GrapevineTestContext, num_users: usize) -> Vec<GrapevineAccount> {
+    let mut users = vec![];
+    for i in 0..num_users {
+        let username = format!("user_{}", i);
+        let user = GrapevineAccount::new(username.into());
+        let request = build_create_user_request(&user);
+        http_create_user(&context, &request).await;
+        users.push(user);
+    }
+    users
+}
+
+/**
+ * Builds chain of relationships between accounts
+ *
+ * @param accounts - the accounts to build relationships between
+ */
+pub async fn relationship_chain(
+    context: &GrapevineTestContext,
+    accounts: &mut Vec<GrapevineAccount>,
+) {
+    for i in 0..accounts.len() - 1 {
+        let request = accounts[i]
+            .new_relationship_request(accounts[i + 1].username(), &accounts[i + 1].pubkey());
+        http_add_relationship(&context, &mut accounts[i], &request).await;
+        let request =
+            accounts[i + 1].new_relationship_request(accounts[i].username(), &accounts[i].pubkey());
+        http_add_relationship(&context, &mut accounts[i + 1], &request).await;
+    }
 }
--- crates/grapevine_server/src/tests/user.rs
+++ crates/grapevine_server/src/tests/user.rs
@@ -1,14 +1,21 @@
 use crate::mongo::GrapevineDB;
 use crate::tests::{
     helpers::{build_create_user_request, GrapevineTestContext},
-    http::http_create_user,
+    http::{
+        http_add_relationship, http_create_user, http_emit_nullifier, http_get_nullifier_secret,
+    },
 };
-use grapevine_common::account::GrapevineAccount;
+use grapevine_common::{
+    account::GrapevineAccount,
+    compat::{convert_ff_ce_to_ff, ff_ce_to_le_bytes},
+};
 use rocket::http::Status;
 
 #[cfg(test)]
 mod user_creation_tests {
     use super::*;
+
+    use grapevine_common::crypto::pubkey_to_address;
 
     #[rocket::async_test]
     pub async fn test_add_user() {
@@ -28,5 +35,211 @@ mod user_creation_tests {
         assert_eq!(message, expected_message);
 
         // todo: additional verification of user existence with other routes
+    }
+
+    #[rocket::async_test]
+    pub async fn test_add_user_no_duplicate() {
+        // Setup
+        let context = GrapevineTestContext::init().await;
+        GrapevineDB::drop("grapevine_mocked").await;
+        // Create and enroll a grapevine account
+        let username = "User_A";
+        let user = GrapevineAccount::new(username.into());
+        let payload = build_create_user_request(&user);
+        _ = http_create_user(&context, &payload).await;
+
+        // try with duplicate user
+        let (code, message) = http_create_user(&context, &payload).await;
+        assert_eq!(code, Status::Conflict.code);
+        assert_eq!(message, String::from("{\"UserExists\":\"User_A\"}"));
+
+        // try with duplicate username
+        let user_duplicate_name = GrapevineAccount::new(username.into());
+        let payload = build_create_user_request(&user_duplicate_name);
+        let (code, message) = http_create_user(&context, &payload).await;
+        assert_eq!(code, Status::Conflict.code);
+        assert_eq!(message, String::from("{\"UsernameExists\":\"User_A\"}"));
+
+        // try with duplicate pubkey
+        let user_duplicate_pubkey =
+            GrapevineAccount::from_repr("User_B".into(), *user.private_key_raw(), 0);
+        let payload = build_create_user_request(&user_duplicate_pubkey);
+        let (code, message) = http_create_user(&context, &payload).await;
+        assert_eq!(code, Status::Conflict.code);
+        let pubkey = format!("0x{}", hex::encode(user.pubkey().compress()));
+        let expected_message = format!("{{\"PubkeyExists\":\"{}\"}}", pubkey);
+        assert_eq!(message, expected_message);
+    }
+
+    #[rocket::async_test]
+    pub async fn test_add_user_bad_proof_output() {
+        // Setup
+        let context = GrapevineTestContext::init().await;
+        GrapevineDB::drop("grapevine_mocked").await;
+        // Create a request where proof creator is different from asserted pubkey
+        let username = "User_A";
+        let user = GrapevineAccount::new(username.into());
+        let mut payload = build_create_user_request(&user);
+        let user_2 = GrapevineAccount::new(username.into());
+        payload.pubkey = user_2.pubkey().compress();
+        let (code, message) = http_create_user(&context, &payload).await;
+        let expected_scope =
+            hex::encode(convert_ff_ce_to_ff(&pubkey_to_address(&user_2.pubkey())).to_bytes());
+        let expected_message = format!(
+            "{{\"ProofFailed\":\"Expected identity scope to equal 0x{}\"}}",
+            expected_scope
+        );
+        assert_eq!(code, Status::BadRequest.code);
+        assert_eq!(message, expected_message);
+    }
+
+    // todo: check malformed inputs
+}
+
+#[cfg(test)]
+mod relationship_tests {
+    use super::*;
+
+    #[rocket::async_test]
+    pub async fn test_relationship_creation() {
+        let context = GrapevineTestContext::init().await;
+        GrapevineDB::drop("grapevine_mocked").await;
+        // Create a request where proof creator is different from asserted pubkey
+        let mut user_a = GrapevineAccount::new("user_a".into());
+
+        let mut user_b = GrapevineAccount::new("user_b".into());
+
+        let user_request_a = build_create_user_request(&user_a);
+        let user_request_b = build_create_user_request(&user_b);
+        http_create_user(&context, &user_request_a).await;
+        http_create_user(&context, &user_request_b).await;
+
+        // add relationship as user_a to user_b
+        let request = user_a.new_relationship_request(user_b.username(), &user_b.pubkey());
+
+        http_add_relationship(&context, &mut user_a, &request).await;
+
+        // accept relation from user_a as user_b
+        let request = user_b.new_relationship_request(user_a.username(), &user_a.pubkey());
+        let expected_nullifier_secret_ciphertext = request.nullifier_secret_ciphertext;
+        http_add_relationship(&context, &mut user_b, &request).await;
+
+        // check stored nullifier secret integrity
+        let nullifier_secret_ciphertext =
+            http_get_nullifier_secret(&context, &mut user_b, user_a.username()).await;
+        let expected_secret = user_b.decrypt_nullifier_secret(expected_nullifier_secret_ciphertext);
+        let empirical_secret = user_b.decrypt_nullifier_secret(nullifier_secret_ciphertext);
+        assert_eq!(expected_secret, empirical_secret);
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_reject_relationship() {
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_no_relationship_with_self() {
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_reject_active_relationship() {
+        // nullifiy, don't reject
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_reject_nonexistent_relationship() {
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_act_nullified_relationship() {
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_request_already_active_relationship() {
+        todo!("Unimplemented")
+    }
+
+    #[rocket::async_test]
+    pub async fn test_nullifier_emission() {
+        let context = GrapevineTestContext::init().await;
+        GrapevineDB::drop("grapevine_mocked").await;
+        // Create a request where proof creator is different from asserted pubkey
+        let mut user_a = GrapevineAccount::new("user_a".into());
+
+        let mut user_b = GrapevineAccount::new("user_b".into());
+
+        let user_request_a = build_create_user_request(&user_a);
+        let user_request_b = build_create_user_request(&user_b);
+        http_create_user(&context, &user_request_a).await;
+        http_create_user(&context, &user_request_b).await;
+
+        // add relationship as user_a to user_b
+        let user_a_relationship_request =
+            user_a.new_relationship_request(user_b.username(), &user_b.pubkey());
+
+        http_add_relationship(&context, &mut user_a, &user_a_relationship_request).await;
+
+        // accept relation from user_a as user_b
+        let user_b_relationship_request =
+            user_b.new_relationship_request(user_a.username(), &user_a.pubkey());
+
+        http_add_relationship(&context, &mut user_b, &user_b_relationship_request).await;
+
+        let encrypted_nullifier_secret =
+            http_get_nullifier_secret(&context, &mut user_a, user_b.username()).await;
+
+        let nullifier_secret = user_a.decrypt_nullifier_secret(encrypted_nullifier_secret);
+
+        // emit nullifier as user_a
+        let code = http_emit_nullifier(
+            &context,
+            ff_ce_to_le_bytes(&nullifier_secret),
+            &mut user_a,
+            user_b.username(),
+        )
+        .await;
+        println!("Code: {}", code);
+        let expected_code = Status::Created.code;
+        assert_eq!(
+            expected_code, code,
+            "Expected HTTP::Created on nullifier emission"
+        );
+
+        // confirm relationship now has emitted nullifier
+        // TODO: FIX
+        // let relationship =
+        //     http_get_relationship(&context, user_b.username(), user_a.username()).await;
+        // assert!(
+        //     relationship.emitted_nullifier.is_some(),
+        //     "No nullifier emitted"
+        // );
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_nullify_pending_relationship() {
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_nullify_nullified_relationship() {
+        todo!("Unimplemented")
+    }
+
+    #[ignore]
+    #[rocket::async_test]
+    pub async fn test_cannot_nullify_nonexistent_relationship() {
+        todo!("Unimplemented")
     }
 }
